9c60c493bfa8563b1238c98851be671b2e4332ff
peterpk1 <phatsawut.phuket1@gmail.com>
2025-05-08 02:38:17 +0700

    fix prtteier

--- kanban_board_backend/eslint.config.mjs
+++ kanban_board_backend/eslint.config.mjs
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   {
-    ignores: ['eslint.config.mjs'],
+    ignores: ['dist/**/*', 'node_modules/**/*','eslint.config.mjs'],
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
@@ -28,7 +28,11 @@ export default tseslint.config(
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+      '@typescript-eslint/no-unsafe-argument': 'warn',
+      "prettier/prettier": [ "error",
+        { "endOfLine": "auto" }
+]
+
     },
   },
 );
